60ea53c4f2afa07cc9f6d99d94514510578b20f1
kailas dandale <kailas.dandale@neosoftmail.com>
2023-01-11 10:02:56 +0530

    first commit

---
+++ src/main/java/com/javatechie/us/UserServiceApplication.java
@@ -0,0 +1,23 @@
+package com.javatechie.us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+@RestController
+
+public class UserServiceApplication {
+
+       public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
